6b91ff808b6b07fa651b54aefc7dbe6f5e8dba6d
yaman <c95029@gmail.com>
2024-07-21 01:41:41 +0900

    remove user avatar field

--- src/main/java/vaadin/gradle/starter/data/User.java
+++ src/main/java/vaadin/gradle/starter/data/User.java
@@ -22,9 +22,6 @@ public class User extends AbstractEntity {
     @Enumerated(EnumType.STRING)
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<Role> roles;
-    @Lob
-    @Column(length = 1000000)
-    private byte[] profilePicture;
 
     public String getUsername() {
         return username;
@@ -49,12 +46,6 @@ public class User extends AbstractEntity {
     }
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
-    }
-    public byte[] getProfilePicture() {
-        return profilePicture;
-    }
-    public void setProfilePicture(byte[] profilePicture) {
-        this.profilePicture = profilePicture;
     }
 
 }
